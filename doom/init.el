8571237ee30f002e4ff5c0715949d540c417035e
Guilherme Parpinelli <guilheerme.p@gmail.com>
2023-04-02 19:56:32 +0100

    feat: add tree-sitter

--- doom/init.el
+++ doom/init.el
@@ -15,172 +15,170 @@
 ;;      directory (for easy access to its source code).
 
 (doom! :input
-       ;;bidi             
+       ;;bidi
        ;;chinese
        ;;japanese
-       ;;layout           
+       ;;layout
 
        :completion
-       company          
-       ;;helm             
-       ;;ido              
-       ;;ivy              
-       (vertico
-        +childframe)    
+       company
+       ;;helm
+       ;;ido
+       ;;ivy
+       (vertico +childframe)
 
        :ui
-       ;;deft             
-       doom             
-       doom-dashboard   
-       doom-quit        
-       ;;(emoji +unicode) 
-       hl-todo          
+       ;;deft
+       doom
+       doom-dashboard
+       doom-quit
+       ;;(emoji +unicode)
+       hl-todo
        ;;hydra
-       ;;indent-guides    
-       ;;ligatures        
-       ;;minimap          
-       modeline         
-       nav-flash        
-       ;;neotree          
-       ophints          
-       (popup +defaults)  
-       ;;tabs             
-       treemacs         
-       ;;unicode          
-       (vc-gutter
-         +diffhl
-         +pretty)
-       vi-tilde-fringe  
-       ;;window-select    
-       workspaces       
+       ;;indent-guides
+       ;;ligatures
+       ;;minimap
+       modeline
+       nav-flash
+       ;;neotree
+       ophints
+       (popup +defaults)
+       ;;tabs
+       treemacs
+       ;;unicode
+       (vc-gutter +diffhl +pretty)
+       vi-tilde-fringe
+       ;;window-select
+       workspaces
        zen
 
        :editor
        (evil +everywhere)
-       file-templates   
-       fold             
+       file-templates
+       fold
        (format +onsave)
-       ;;god              
-       ;;lispy            
+       ;;god
+       ;;lispy
        multiple-cursors
-       ;;objed            
-       ;;parinfer         
+       ;;objed
+       ;;parinfer
        rotate-text
-       snippets         
+       snippets
        word-wrap
 
        :emacs
-       dired            
-       electric         
+       dired
+       electric
        ibuffer
        (undo +tree)
-       vc               
+       vc
 
        :term
-       ;;eshell           
-       ;;shell            
-       ;;term             
+       ;;eshell
+       ;;shell
+       ;;term
        vterm
 
        :checkers
-       syntax             
+       syntax
        ;;(spell +flyspell)
-       ;;grammar          
+       ;;grammar
 
        :tools
        ;;ansible
-       ;;biblio           
+       ;;biblio
        debugger
        ;;direnv
        (docker +lsp)
        editorconfig
-       ;;ein              
-       (eval +overlay)    
+       ;;ein
+       (eval +overlay)
        gist
-       lookup             
+       lookup
        (lsp)
        (magit +forge)
        make
-       ;;pass             
+       ;;pass
        pdf
-       ;;prodigy          
+       ;;prodigy
        rgb
-       ;;taskrunner       
-       ;;terraform        
-       ;;tmux             
+       ;;taskrunner
+       ;;terraform
+       ;;tmux
        tree-sitter
-       ;;upload           
+       ;;upload
 
        :os
-       (:if IS-MAC macos) 
+       (:if IS-MAC macos)
        tty
 
        :lang
-       ;;agda             
-       ;;beancount        
-       (cc +lsp)
-       ;;clojure          
-       ;;common-lisp      
-       ;;coq              
-       ;;crystal          
-       ;;csharp           
-       ;;data             
-       ;;(dart +flutter)  
+       ;;agda
+       ;;beancount
+       (cc +tree-sitter +lsp)
+       ;;clojure
+       ;;common-lisp
+       ;;coq
+       ;;crystal
+       ;;csharp
+       ;;data
+       ;;(dart +flutter)
        ;;dhall
-       ;;elixir           
-       ;;elm              
-       emacs-lisp       
-       ;;erlang           
-       ;;ess              
+       ;;elixir
+       ;;elm
+       emacs-lisp
+       ;;erlang
+       ;;ess
        ;;factor
-       ;;faust            
-       ;;fortran          
-       ;;fsharp           
-       ;;fstar            
-       ;;gdscript         
-       (go +lsp)
-       ;;(graphql +lsp)   
-       ;;(haskell +lsp)   
-       ;;hy               
-       ;;idris            
-       json
-       ;;(java +lsp)      
-       javascript
-       ;;julia            
-       ;;kotlin           
-       ;;latex            
-       ;;lean             
-       ;;ledger           
+       ;;faust
+       ;;fortran
+       ;;fsharp
+       ;;fstar
+       ;;gdscript
+       (go +tree-sitter +lsp)
+       ;;(graphql +lsp)
+       ;;(haskell +lsp)
+       ;;hy
+       ;;idris
+       (json +tree-sitter +lsp)
+       ;;(java +lsp)
+       (javascript +tree-sitter +lsp)
+       ;;julia
+       ;;kotlin
+       ;;latex
+       ;;lean
+       ;;ledger
        lua
-       markdown         
-       ;;nim              
-       ;;nix              
-       ;;ocaml            
+       markdown
+       ;;nim
+       ;;nix
+       ;;ocaml
        (org +pretty)
-       ;;php              
-       ;;plantuml         
-       ;;purescript       
+       ;;php
+       ;;plantuml
+       ;;purescript
        (python
         +pyenv
         +lsp
         +pyright
-        +poetry)
+        +poetry
+        +tree-sitter)
        ;;qt
-       ;;racket           
-       ;;raku             
+       ;;racket
+       ;;raku
        rest
-       ;;rst              
-       ;;(ruby +rails)    
-       (rust +lsp)
-       ;;scala            
-       ;;(scheme +guile)  
-       (sh +lsp)
+       ;;rst
+       ;;(ruby +rails)
+       (rust +tree-sitter +lsp)
+       ;;scala
+       ;;(scheme +guile)
+       (sh +tree-sitter +lsp)
        ;;sml
-       ;;solidity         
-       ;;swift            
-       ;;terra            
-       (web +lsp)
-       (yaml +lsp)
+       ;;solidity
+       ;;swift
+       ;;terra
+       (web +tree-sitter +lsp)
+       (yaml +tree-sitter +lsp)
        ;;zig
 
        :email
@@ -191,9 +189,9 @@
        :app
        ;;calendar
        ;;emms
-       ;;everywhere       
-       ;;irc              
-       ;;(rss +org)       
+       ;;everywhere
+       ;;irc
+       ;;(rss +org)
        ;;twitter
 
        :config
