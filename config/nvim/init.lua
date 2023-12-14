03fea7e8e12a373d14689671514ab614c8f5b17d
Guilherme Parpinelli <guilheerme.p@gmail.com>
2023-12-14 12:13:29 +0000

    nvim: add lsp support

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -57,6 +57,27 @@ require('lazy').setup({
 
   'TimUntersberger/neogit',
 
+{
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  }
+
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
